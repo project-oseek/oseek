88f2ec9e68bff304a60d2cd93f5de1d493654262
bytrustu <bytrustu@gmail.com>
2023-05-28 12:33:23 +0900

    refactor: ui 패키지 jest, vite의 name 속성 보완

--- libs/ui/jest.config.ts
+++ libs/ui/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'button',
+  displayName: 'ui',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
--- libs/ui/vite.config.ts
+++ libs/ui/vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
-      name: 'button',
+      name: 'ui',
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forgot to update your package.json as well.
