c4ad479c2eacbc4db05698ac344d556ce9f3ad1e
bytrustu <bytrustu@gmail.com>
2023-06-11 03:25:43 +0900

    feat: LoginHeader, LoginHeading 컴폰너트 추가

---
+++ apps/client/components/login/LoginHeading.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from 'react';
+import { Typography } from '@oseek/ui';
+
+type Props = {
+  title: ReactElement;
+  description?: ReactElement;
+};
+
+const LoginHeading = ({ title, description }: Props) => {
+  return (
+    <div style={{ padding: '0 24px', margin: '24px 0 0 0' }}>
+      <Typography as="h1" variant="H1">
+        {title}
+      </Typography>
+      {description && (
+        <Typography as="p" variant="B2" style={{ marginTop: '8px' }}>
+          {description}
+        </Typography>
+      )}
+    </div>
+  );
+};
+
+export default LoginHeading;
