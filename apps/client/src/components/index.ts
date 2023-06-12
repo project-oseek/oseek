e11a18d7e286f0a60f7bd000a92e5bdb771da150
Jae Kim <9028364@gmail.com>
2023-06-12 15:26:19 +0900

    feat: Spacing 컴포넌트 추가

--- apps/client/src/components/index.ts
+++ apps/client/src/components/index.ts
@@ -9,3 +9,4 @@ export * from './layout/index';
 export * from './login/index';
 export * from './main/index';
 export * from './section/index';
+export * from './spacing/index';
---
+++ apps/client/src/components/spacing/Spacing.css.ts
@@ -0,0 +1,5 @@
+import { style } from '@vanilla-extract/css';
+
+export const Basic = style({
+  width: '100%',
+});
