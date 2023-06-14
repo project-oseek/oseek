b26b02ef2386728c204f5adb7f7270f8134aa843
Jae Kim <9028364@gmail.com>
2023-06-15 02:46:50 +0900

    feat: 메인화면 -> cuisine 경로 설정 및 header 수정

--- apps/client/src/components/main/menu/MenuCategory.tsx
+++ apps/client/src/components/main/menu/MenuCategory.tsx
@@ -1,39 +1,42 @@
-'use client';
-
 import React from 'react';
 import Image from 'next/image';
 
-import * as S from './MenuCategory.css';
+import Link from 'next/link';
 import { Typography } from '@oseek/ui';
+import * as S from './MenuCategory.css';
 import Asian from './images/asian.png';
 import Cafe from './images/cafe.png';
 import Japanese from './images/japanese.png';
 import Korean from './images/korean.png';
 import Western from './images/western.png';
 
+const generatePath = (path: string) => {
+  return `/cuisine/${path}`;
+};
+
 export const MenuCategory = () => {
   return (
     <div className={S.Container}>
-      <div className={S.Category}>
+      <Link className={S.Category} href={generatePath('korean')}>
         <Image src={Korean} alt="menu-category" width={96} height={96} />
         <Typography variant="B1">한식</Typography>
-      </div>
-      <div className={S.Category}>
+      </Link>
+      <Link className={S.Category} href={generatePath('western')}>
         <Image src={Western} alt="menu-category" width={96} height={96} />
         <Typography variant="B1">양식</Typography>
-      </div>
-      <div className={S.Category}>
+      </Link>
+      <Link className={S.Category} href={generatePath('japanese')}>
         <Image src={Japanese} alt="menu-category" width={96} height={96} />
         <Typography variant="B1">일식</Typography>
-      </div>
-      <div className={S.Category}>
+      </Link>
+      <Link className={S.Category} href={generatePath('asian')}>
         <Image src={Asian} alt="menu-category" width={96} height={96} />
-        <Typography variant="B1">한식</Typography>
-      </div>
-      <div className={S.Category}>
+        <Typography variant="B1">아시안</Typography>
+      </Link>
+      <Link className={S.Category} href={generatePath('cafe-dessert')}>
         <Image src={Cafe} alt="menu-category" width={96} height={96} />
         <Typography variant="B1">카페/디저트</Typography>
-      </div>
+      </Link>
     </div>
   );
 };
