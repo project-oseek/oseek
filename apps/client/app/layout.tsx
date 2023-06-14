5bc6f3442650a8d0bc0962c6fc516cb63e50c31e
bytrustu <bytrustu@gmail.com>
2023-06-15 02:50:28 +0900

    feat: Overlay 로그인페이지 적용 테스트

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -1,19 +1,21 @@
 import './global.css';
 import { PropsWithChildren } from 'react';
-import { Layout } from '@oseek/ui';
+import { Layout, OverlayProvider } from '@oseek/ui';
 
 export const metadata = {
   title: {
     default: '오늘의 식사',
-    template: '오늘의 식사 | %s'
-  }
+    template: '오늘의 식사 | %s',
+  },
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ko">
       <body>
-        <Layout>{children}</Layout>
+        <Layout>
+          <OverlayProvider>{children}</OverlayProvider>
+        </Layout>
       </body>
     </html>
   );
--- apps/client/src/components/login/page/LoginPage.tsx
+++ apps/client/src/components/login/page/LoginPage.tsx
@@ -1,10 +1,27 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 import { Button, SnsKakao } from '@oseek/ui';
 
+import { useAlert, useConfirm } from '@oseek/lib';
 import * as S from './LoginPage.css';
 
 export const LoginPage = () => {
+  const alert = useAlert();
+  const confirm = useConfirm();
+  const handleAlertClick = async () => {
+    alert({ title: '안녕하세요.' }, { closeOnOverlayClick: true });
+  };
+  const handleConfirmClick = async () => {
+    const resultConfirm = await confirm({
+      title: '프로필 수정을 그만두시겠어요?',
+      description: '작성된 내용을 저장해주세요.',
+      submitText: '저장하기',
+      closeText: '나가기',
+    });
+    console.log('resultConfirm', resultConfirm);
+  };
   return (
     <>
       <Image className={S.logo} src="/images/login-logo.png" width={208} height={84} alt="로고 이미지" />
@@ -13,7 +30,9 @@ export const LoginPage = () => {
           <SnsKakao className={S.kakaoIcon} width={24} height={24} />
           <span>카카오로 시작하기</span>
         </Button>
-        <Button buttonType="Line">비회원으로 시작하기</Button>
+        <Button buttonType="Line" onClick={handleConfirmClick}>
+          비회원으로 시작하기
+        </Button>
       </div>
     </>
   );
