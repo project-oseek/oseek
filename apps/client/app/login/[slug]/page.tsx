c977e5e33d4ef38d71aa3c4866c97effe4f777e6
bytrustu <bytrustu@gmail.com>
2023-06-23 02:55:18 +0900

    feat: Axios 설정 및 401 상태코드 경우 토큰 재발행 로직 추가

--- apps/client/app/login/[slug]/page.tsx
+++ apps/client/app/login/[slug]/page.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { SettingKeywordPage, SettingLocationPage, SettingMatchPage, SettingNamePage } from '../../../src/components/login/page';
+import { SettingKeywordPage, SettingLocationPage, SettingMatchPage, SettingNamePage } from '@components/login';
 
 type Props = {
   params: {
-    slug: 'setting-name' | 'setting-keyword' | 'setting-location' | 'setting-match';
+    slug: 'setting-name' | 'setting-location' | 'setting-match';
   };
 };
 
 const SettingPages = {
   'setting-name': SettingNamePage,
-  'setting-keyword': SettingKeywordPage,
+  // 'setting-keyword': SettingKeywordPage,
   'setting-location': SettingLocationPage,
   'setting-match': SettingMatchPage,
 };
