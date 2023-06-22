befa7e63685a6f38e7b254114fc3a649e85185c3
bytrustu <bytrustu@gmail.com>
2023-06-23 02:55:54 +0900

    feat: localStorage utils 및 constant 추가

---
+++ libs/lib/src/utils/storage.ts
@@ -0,0 +1,27 @@
+export const getLocalStorageItem = <T>(key: string): T | null => {
+  if (typeof window !== 'undefined') {
+    const item = localStorage.getItem(key);
+    if (item) {
+      try {
+        return JSON.parse(item) as T;
+      } catch (error) {
+        console.error(`${key} 파싱에 실패하였습니다. Error: ${error}`);
+        return null;
+      }
+    }
+    return null;
+  }
+  return null;
+};
+
+export const setLocalStorageItem = <T>(key: string, value: T): void => {
+  if (typeof window !== 'undefined') {
+    try {
+      const serializedValue = JSON.stringify(value);
+      localStorage.setItem(key, serializedValue);
+    } catch (error) {
+      const errorMessage = `${key} 저장에 실패하였습니다. Error: ${error}`;
+      (() => new Error(errorMessage))();
+    }
+  }
+};
