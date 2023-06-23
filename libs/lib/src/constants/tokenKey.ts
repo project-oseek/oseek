9fe54eca7ff1a4a85d339cd6f6ec49615d226f00
bytrustu <bytrustu@gmail.com>
2023-06-24 02:30:03 +0900

    feat: localStorage, tokenKey 항목 추가

--- libs/lib/src/constants/tokenKey.ts
+++ libs/lib/src/constants/tokenKey.ts
@@ -1,2 +1,4 @@
 export const ACCESS_TOKEN_KEY = 'accessToken';
 export const REFRESH_TOKEN_KEY = 'refreshToken';
+export const NICKNAME_KEY = 'nickname';
+export const LOCATION_KEY = 'location';
--- libs/lib/src/utils/storage.ts
+++ libs/lib/src/utils/storage.ts
@@ -1,3 +1,5 @@
+import { ACCESS_TOKEN_KEY, LOCATION_KEY, NICKNAME_KEY, REFRESH_TOKEN_KEY } from '../constants';
+
 export const getLocalStorageItem = <T>(key: string): T | null => {
   if (typeof window !== 'undefined') {
     const item = localStorage.getItem(key);
@@ -24,4 +26,24 @@ export const setLocalStorageItem = <T>(key: string, value: T): void => {
       (() => new Error(errorMessage))();
     }
   }
+};
+
+export const getAccessTokenByLocalStorage = (): string | null => {
+  const accessToken = getLocalStorageItem<string>(ACCESS_TOKEN_KEY);
+  return accessToken || null;
+};
+
+export const getRefreshTokenByLocalStorage = (): string | null => {
+  const refreshToken = getLocalStorageItem<string>(REFRESH_TOKEN_KEY);
+  return refreshToken || null;
+};
+
+export const getNickNameByLocalStorage = (): string | null => {
+  const nickname = getLocalStorageItem<string>(NICKNAME_KEY);
+  return nickname || null;
+};
+
+export const getLocationByLocalStorage = (): string | null => {
+  const location = getLocalStorageItem<string>(LOCATION_KEY);
+  return location || null;
 };
